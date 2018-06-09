cc5737b79515c3fb8a56812ec25a223c33c3661f
root <root@11CPU0276L.amritavidya.edu>
2018-06-09 16:36:42 +0530

    Added file

---
+++ sasi/blog/views.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.views.generic import ListView
+from . import models
+
+# Create your views here.
+class HomePageView(ListView):
+	model=models.Post
+	template_name='blog/blog_post.html'
